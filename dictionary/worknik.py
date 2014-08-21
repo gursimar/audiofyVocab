e0ab7d3e4dd7dc23433c515481ddda774fff7144
simar <simar.i3r@gmail.com>
2014-08-21 17:37:26 +0530

    added worknik example dictionary

---
+++ dictionary/worknik.py
@@ -0,0 +1,11 @@
+from wordnik import *
+apiUrl = 'http://api.wordnik.com/v4'
+apiKey = '960542d38a1d63153361539342c38085a378dcecf6fc3578f'
+client = swagger.ApiClient(apiKey, apiUrl)
+
+wordApi = WordApi.WordApi(client)
+example = wordApi.getTopExample('irony')
+print example.text
+
+
+
