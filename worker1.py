d8b87dbbc66430bb80e6b8b301e91f491e8413a7
simar <simar.i3r@gmail.com>
2014-09-04 16:41:56 +0530

    updated read

--- worker1.py
+++ worker1.py
@@ -1,23 +1,33 @@
 from dictionary.WordnixDictionary import WordnixDictionary
 from ttsengine.GoogleTTS import GoogleTTS
+output_dir = 'output/'
+input_file = 'words.txt'
 
-word = 'dejected'
+f = open(output_dir + input_file, 'r')
+wordstext = f.read()
+f.close
+#print wordstext
+words = wordstext.split('\r\n')
+#print words
+for word in words:
+	#print word
+	#word = 'dejected'
 
+	filename = output_dir + word + '.mp3'
+	f = open(filename, 'w')
+			
+	gtts = GoogleTTS()
+	wndic = WordnixDictionary()
 
-filename = 'output/' + word + '.mp3'
-f = open(filename, 'w')
-		
-gtts = GoogleTTS()
-wndic = WordnixDictionary()
+	sentence = ''
+	sentence = sentence + word + '.'
 
-sentence = ''
-sentence = sentence + word + '.'
+	definitions = wndic.getDefinitions(word)
+	for i,v in enumerate(definitions):
+		sentence = sentence + v.text
+		break
 
-definitions = wndic.getDefinitions(word)
-for i,v in enumerate(definitions):
-	sentence = sentence + v.text
-
-print word + ' ' + sentence + '\n'
-data = gtts.convertAudioMP3(sentence)
-f.write (data)
-f.close()
+	print sentence + '\n'
+	data = gtts.convertAudioMP3(sentence)
+	f.write (data)
+	f.close()
